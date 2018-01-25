5b6db4940597257a53fdfa28ff4cb47d94571561
Richard Liu <richy.liu.2002@gmail.com>
2018-01-24 21:51:11 -0800

    end of day commit (displayweek wip)

--- js/main.js
+++ js/main.js
@@ -13,34 +13,68 @@ let habits = {
     }
 };
 
-for (let val of Object.values(habits)) {
-    addHabit(val.name, val.history, val.start, history => {
-        // add new history to firebase
-    });
-}
-
-
-
-/**
- * Adds a habit onscreen with the corresponding event handlers for change
- * @param {String}   name     Name of the habit to track
- * @param {Number[]} history  If the tracked item has been completed
- * @param {Date}     start    When tracking started
- * @param {Function} onchange Called when history is change
- */
-function addHabit(name, history, start, onchange) {
+for (let key of Object.keys(habits)) {
+    let val = habits[key];
     $('#out').append(`
         <div class="habit-wrapper">
             <div class="habit-name">
-                ${name}
+                ${val.name}
             </div>
             <div class="history-bar">
                 <div class="history-back"><i class="far fa-angle-left"></i></div>
-                <div class="history-wrapper">
-                    ${history.map(value => `<span class="history-block ${value ? 'green' : 'red'}"></span>`).join('')}
-                </div>
+                <div class="history-wrapper" id="${key}" data-number="1"></div>
                 <div class="history-forward"><i class="far fa-angle-right"></i></div>
             </div>
         </div>
     `);
+    
+    updateDisplayWeek(key, 1);
+}
+
+
+
+// shift the history forward or backwards by one week
+$('.history-back,.history-forward').click(e => {
+    let historyWrapper = $(e.currentTarget).siblings('.history-wrapper');
+    let currentNumber = historyWrapper.data('number');
+    
+    if ($(e.currentTarget).hasClass('history-back')) {
+        updateDisplayWeek(historyWrapper.attr('id'), currentNumber + 1);
+    } else {
+        updateDisplayWeek(historyWrapper.attr('id'), currentNumber - 1);
+    }
+    
+});
+
+
+/**
+ * Update the displayed week for the given date and key
+ * @param  {String} key        Key of the habit to be updated
+ * @param  {Number} weekNumber What week to display (1 for most recent week)
+ */
+function updateDisplayWeek(key, weekNumber) {
+    // ensure weekNumber is greater than 1
+    weekNumber = weekNumber > 1 ? weekNumber : 1;
+    let habit = habits[key];
+    let end = habit.start.addDays(habit.history.length - 1);
+    
+    if (weekNumber > 1) {
+        
+    } else {
+        let displayedWeek = habit.slice()
+    }
+    
+    
+    
+    $(`#${key}`).data('number', weekNumber).html(
+        displayWeek(start, history, 1).map(value => `<span class="history-block ${value ? 'green' : 'red'}"></span>`).join('')
+    );
+}
+
+
+
+Date.prototype.addDays = function(days) {
+    var dat = new Date(this.valueOf());
+    dat.setDate(dat.getDate() + days);
+    return dat;
 }
