33fcae38fec29746fce9a32f0d782df92ce657fc
Richard Liu <richy.liu.2002@gmail.com>
2018-01-25 19:21:25 -0800

    add ability to moving viewing history

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
 /* global $ firebase */
 
+
+Date.prototype.addDays = function(days) {
+    let d = new Date(this.valueOf());
+    d.setDate(d.getDate() + days);
+    return d;
+};
+
+
+
+let COLORS = {
+    0: 'green',
+    1: 'red',
+    2: 'white'
+};
+
+
+
 let habits = {
     'key1': {
         name: 'Drink water',
         history: [1, 0, 1, 0, 0, 1, 1, 1],
-        start: new Date('1/13/2018')
+        start: new Date('1/18/2018')
     },
     'key2': {
         name: 'Eat fruit',
         history: [1, 1, 0, 1, 1, 1, 1, 1],
-        start: new Date('1/13/2018')
+        start: new Date('1/18/2018')
     }
 };
 
@@ -22,7 +39,7 @@ for (let key of Object.keys(habits)) {
             </div>
             <div class="history-bar">
                 <div class="history-back"><i class="far fa-angle-left"></i></div>
-                <div class="history-wrapper" id="${key}" data-number="1"></div>
+                <div class="history-wrapper" id="${key}"></div>
                 <div class="history-forward"><i class="far fa-angle-right"></i></div>
             </div>
         </div>
@@ -38,12 +55,10 @@ $('.history-back,.history-forward').click(e => {
     let historyWrapper = $(e.currentTarget).siblings('.history-wrapper');
     let currentNumber = historyWrapper.data('number');
     
-    if ($(e.currentTarget).hasClass('history-back')) {
+    if ($(e.currentTarget).hasClass('history-back'))
         updateDisplayWeek(historyWrapper.attr('id'), currentNumber + 1);
-    } else {
+    else
         updateDisplayWeek(historyWrapper.attr('id'), currentNumber - 1);
-    }
-    
 });
 
 
@@ -56,25 +71,35 @@ function updateDisplayWeek(key, weekNumber) {
     // ensure weekNumber is greater than 1
     weekNumber = weekNumber > 1 ? weekNumber : 1;
     let habit = habits[key];
-    let end = habit.start.addDays(habit.history.length - 1);
     
-    if (weekNumber > 1) {
-        
-    } else {
-        let displayedWeek = habit.slice()
+    let his = habit.history;
+    let end = habit.start.addDays(his.length - 1);
+    let lastSunday = his.length - end.getDay() - 1;
+    let displayWeek = [];
+    
+    let beginSlice = lastSunday - 7*(weekNumber-1);
+    let endSlice = beginSlice + 7;
+    
+    if ((beginSlice < 0 && endSlice < 0) || (beginSlice > his.length-1 && endSlice > his.length-1)) {
+        // weekNumber out of bounds, exiting function
+        console.error(`Week number ${weekNumber} out of bounds for key ${key}`);
+        return;
     }
     
-    
+    console.log(beginSlice, endSlice);
+    if (beginSlice < 0) {
+        displayWeek = his.slice(0, endSlice);
+        // pad arrays with blanks
+        displayWeek.unshift(...Array(8 + beginSlice).fill(2));
+    } else if (endSlice > his.length-1) {
+        displayWeek = his.slice(beginSlice);
+        // pad arrays with blanks
+        displayWeek.push(...Array(7 - his.length + lastSunday).fill(2));
+    } else {
+        displayWeek = his.slice(beginSlice, endSlice);
+    }
     
     $(`#${key}`).data('number', weekNumber).html(
-        displayWeek(start, history, 1).map(value => `<span class="history-block ${value ? 'green' : 'red'}"></span>`).join('')
+        displayWeek.map(value => `<span class="history-block ${COLORS[value]}"></span>`).join('')
     );
-}
-
-
-
-Date.prototype.addDays = function(days) {
-    var dat = new Date(this.valueOf());
-    dat.setDate(dat.getDate() + days);
-    return dat;
 }
