1c46f126b89494df46df723c622c06b4bb911687
Richard Liu <richy.liu.2002@gmail.com>
2018-02-15 21:20:27 +0000

    add detail page

--- js/my-app.js
+++ js/my-app.js
@@ -4,33 +4,29 @@ let app = new Framework7({
     // App root element
     root: '#app',
     // App Name
-    // name: 'My App',
-    // // App id
-    // id: 'com.myapp.test',
-    // // Enable swipe panel
-    // panel: {
-    //     swipe: 'left',
-    // },
-    // // Add default routes
-    // routes: [{
-    //     path: '/about/',
-    //     url: 'about.html',
-    // }]
+    name: 'My App',
+    // App id
+    id: 'com.myapp.test',
+    // Enable swipe panel
+    panel: {
+        swipe: 'left',
+    },
+    // Add default routes
+    routes: [{
+        path: '/detail/',
+        url: 'detail.html',
+    }]
     // ... other parameters
 });
 
-let mainView = app.views.create('.view-main');
+let mainView = app.views.create('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
 
-// $$('button').click(e => {
-//     e.stopPropagation();
-// });
-// 
-// $$('a').click(e => {
-//     e.stopPropagation();
-//     e.preventDefault();
-// });
-
+/*
 let el = document.querySelector('a.item-link');
 let elClone = el.cloneNode(true);
 
 el.parentNode.replaceChild(elClone, el);
+*/
